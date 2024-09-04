c3a202e0bc6f4be80a4cd6446d54abf25016a177
rizzzav <rishavkmr1111@gmail.com>
2024-09-04 17:52:21 +0530

    Add files via upload

---
+++ main.java
@@ -0,0 +1,9 @@
+public class main {
+    public static void main(String[] args) {
+        //output
+        System.out.println("Hello World!"); //using println for changing line   
+        System.out.print("Hello World!\n"); //using \n for changing line
+        System.out.print("Hello World!");
+
+    }
+}
